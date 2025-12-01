c347568786f42cc211ce980f8a18fbc6e626a895
Federico Aguirre <federico.aguirre.cardiel@gmail.com>
2025-11-30 18:36:23 -0600

    Resolves functions exercises

--- exercises/02_functions/functions2.rs
+++ exercises/02_functions/functions2.rs
@@ -1,6 +1,6 @@
 // TODO: Add the missing type of the argument `num` after the colon `:`.
-fn call_me(num:) {
-    for i in 0..num {
+fn call_me(num: u8) {
+    for i: u8 in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
 }
